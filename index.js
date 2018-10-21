1a9e1e27ed8dbba12b8dcad216feef982a7afc99
Faizan Jamil <faizan34@gmail.com>
2018-10-20 22:13:44 -0400

    Added ability to record exercises, removed ability to delete users

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const users = [
-    {id: 1, name: 'faizan'},
+    {id: 1, name: 'faizan', exercises: ''},
     {id: 2, name: 'amy'},
     {id: 3, name: 'don'},
 ];
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(2).required(),
+        exercises: Joi.string().required()
+    }
+    return Joi.validate(user, schema);
+    
+};
+
 app.get('/', (req,res) => {
     res.send('Hello world');
 });
@@ -30,14 +40,15 @@ app.post('/api/users', (req,res) => {
 
     const user = {
         id: users.length + 1,
-        name: req.body.name
+        name: req.body.name,
+        exercises: req.body.exercises
     };
     users.push(user);
     res.send(user);
 });
-
+//lookup user with id specified
 app.put('/api/users/:id', (req,res) =>{
-    //lookup user with id specified
+    
     //if not existing return 404
     let user = users.find(c => c.id === parseInt(req.params.id));
     if (!user) res.status(404).send('The user with the given id was not found');
@@ -52,19 +63,14 @@ app.put('/api/users/:id', (req,res) =>{
     }
     //update user
     //updates name
+    //updates exercises
     //will use for other properties
     user.name = req.body.name;
+    user.exercises = req.body.exercises;
     //return user to client
     res.send(user);
 });
 
-function validateUser(user) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    }
-    return Joi.validate(user, schema);
-    
-};
 
 //get user name corresponding to id entered
 app.get('/api/users/:id', (req,res) =>{
@@ -74,8 +80,8 @@ app.get('/api/users/:id', (req,res) =>{
     //res.send(req.params);
 });
 
-//delete user
-app.delete('api/users/:id', (req, res) => {
+//delete user (Not Functioning)
+/*app.delete('api/users/:id', (req, res) => {
     //look up user
     const user = users.find(c => c.id === parseInt(req.params.id));
     if (!user) res.status(404).send('The user with the given id was not found');
@@ -85,7 +91,7 @@ app.delete('api/users/:id', (req, res) => {
     users.splice(index, 1);
     //return same user
     res.send(user);
-});
+});*/
 //PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
