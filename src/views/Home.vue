7a7c2c7cb0075127da82354436298e9907af1f50
Faizan <faizan34@gmail.com>
2018-12-31 17:02:38 -0500

    creating basic structure of app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" style="width:100px;height:100px">
+    <About msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import About from '@/components/About.vue'
+// import Nav from '@/components/Nav.vue'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    About
   }
 }
 </script>
