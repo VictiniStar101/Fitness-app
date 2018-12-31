f446a76c2e556b9f4def6d41e97802e5a8c4c376
Faizan <faizan34@gmail.com>
2018-12-31 17:23:09 -0500

    added navbar example

---
+++ src/components/About.vue
@@ -0,0 +1,59 @@
+
+<template>
+  <!-- where HTML goes -->
+  <div class="hello">
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'> {{index}}. {{data.skill}}
+        </li>
+      </ul>
+
+      <p v-if="skills.length >= 1"> You have more than one skill</p>
+      <p v-else>You have less than one skill</p>
+    </div>    
+  <!--
+    About
+
+    {{ name }}
+    {{ btnState ? 'The button is disabled' : 'The button is active'}}
+    <button v-on:click="changeName" v-bind:disabled="btnState">Change Name</button>
+  -->
+  </div>
+</template>
+
+<script>
+// where logic goes
+export default {
+  name: 'About',
+  data () {
+    return {
+      skills: [
+        {"skill": "Vue.js"},
+        {"skill": "Python"}
+        
+        ]
+      // name: 'Faizan',
+      // btnState: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+// where CSS goes
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="home">
+    <!--
     <img alt="Vue logo" src="../assets/logo.png" style="width:100px;height:100px">
     <About msg="Welcome to Your Vue.js App"/>
+    -->  
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import About from '@/components/About.vue'
-// import Nav from '@/components/Nav.vue'
 export default {
   name: 'home',
   components: {
