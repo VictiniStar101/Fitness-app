0f130288a8642f2bc9dbb02c859233eeb035a953
Faizan <faizan34@gmail.com>
2019-01-06 15:26:53 -0500

    modified layout of profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,27 +4,42 @@
   <!-- display username -->
   <!-- allow user to add and change nickname -->
     <div class="jumbotron">
-      <button v-if="user==''" type="button" class="btn btn-info">Login here</button>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col" >
+            <h5>Current user information</h5>
+          </div>
+          <div class="col" >
+            <button v-if="user==''" type="button" class="btn btn-info">Login here</button>
+            <div class="user" v-else >
+              <!--You are logged in-->
+              <h5>Profile settings</h5>
+              <form>
+                <div class="form-group">
+                  <label for="nickname">Add/Change Nickname (optional)</label>
+                  <input type="nickname" class="form-control" id="newNickname" aria-describedby="NicknameHelp" placeholder="Enter nickname">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit
+                </button>
 
-      <div class="user" v-else >
-        <!--You are logged in-->
+                <div class="form-group">
+                  <br />
 
-        <form>
-          <div class="form-group">
-            <label for="nickname">Add/Change Nickname (optional)</label>
-            <input type="nickname" class="form-control" id="newNickname" aria-describedby="NicknameHelp" placeholder="Enter nickname">
+                  <label for="exampleInputPassword1">Add Exercise</label>
+                  <input type="exercise" class="form-control" id="exerciseAdded" placeholder="Enter an Exercise">
+                </div>
+                <button type="button" class="btn btn-success">Add</button>
+                <button type="button" class="btn btn-danger">Delete</button>
+                <!--
+                <div class="form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                -->
+              </form>
+            </div>
           </div>
-          <!--<div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-          </div>
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
-          -->
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        </div>
       </div>
     </div>
   </div>
@@ -40,3 +55,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+  .btn-success {
+    margin-right: 30px;
+
+  }
+</style>
+
